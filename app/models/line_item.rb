e1792ce6489deee327342a0d25f8c1dd8e4613c3
thonier <thonier30@gmail.com>
2017-03-10 03:12:12 +0000

    hi

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,11 @@
 class LineItem < ApplicationRecord
-  belongs_to :product
-  belongs_to :cart
+  belongs_to :order, optional: true
+  belongs_to :product, optional: true
+  
+  belongs_to :cart 
+
+  def total_price 
+  product.price * quantity 
+  end 
 end
+
